0ccfc4625fd144e05e0cf18e1d6d1d3e092b1022
HIKARU KITAGAWA <hikaru.kitagawa.j2u@jp.denso.com>
2026-01-29 15:06:43 +0900

    add file

--- detect_sys/main.py
+++ detect_sys/main.py
@@ -33,24 +33,24 @@ def cfg(cfg, keys, default=None):
         cur = cur[k]
     return cur
 
-## Get Config
+## Get ConfigP
 #workdirectory is based on script path
 os.chdir(Path(__file__).resolve().parent)
 
 CFG = load_toml('config.toml')
 
-ENC = cfg(CFG, ('io', 'encoding'), 'CP932') #Encoding
-HDRROW = cfg(CFG, ('io', 'header_row'), 2) #Header_row_count
+ENC         = cfg(CFG, ('io', 'encoding'), 'CP932')                             #Encoding
+HDRROW      = cfg(CFG, ('io', 'header_row'), 2)                                 #Header_row_count
 
-BASE_DIR = cfg(CFG, ('paths', 'base_dir')) #Original_Data_directory
-CSV_GLOB = cfg(CFG, ('paths', 'glob_csv'), '*.csv') #Target_files
-PREV_PATH = cfg(CFG, ('paths', 'prev_path'), 'table/d_tube_assembly.csv') #Previous_Data_file
-CROSS_XLSX = cfg(CFG, ('paths', 'cross_xlsx')) #Cross_Table_file
-OUT_EVENTS = cfg(CFG, ('paths', 'out_events')) #Unmatch_file
-OUTPUT_DIR = cfg(CFG, ('paths', 'output_dir')) #Ouput_directory
-STATE_PATH = cfg(CFG, ('paths', 'state_path'), 'state.json') #Already_processed_file
+BASE_DIR    = cfg(CFG, ('paths', 'base_dir'))                                   #Original_Data_directory
+CSV_GLOB    = cfg(CFG, ('paths', 'glob_csv'), '*.csv')                          #Target_files
+PREV_PATH   = cfg(CFG, ('paths', 'prev_path'), 'table/d_tube_assembly.csv')     #Previous_Data_file
+CROSS_XLSX  = cfg(CFG, ('paths', 'cross_xlsx'))                                 #Cross_Table_file
+OUT_EVENTS  = cfg(CFG, ('paths', 'out_events'))                                 #Unmatch_file
+OUTPUT_DIR  = cfg(CFG, ('paths', 'output_dir'))                                 #Ouput_directory
+STATE_PATH  = cfg(CFG, ('paths', 'state_path'), 'state.json')                   #Already_processed_file
 
-DEBOUNCE_N = int(cfg(CFG, ('logic', 'debounce_n'), 1)) #Switch_debouncing(1:OFF,3>=:ON)
-DUR_MIN = int(cfg(CFG, ('logic', 'duration_min_ms'), 0)) #Duration_minimum_seconds
-DUR_MAX = int(cfg(CFG, ('logic', 'duration_max_ms'), 0)) #Duration_maximum_seconds
+DEBOUNCE_N  = int(cfg(CFG, ('logic', 'debounce_n'), 1))                         #Switch_debouncing(1:OFF,3>=:ON)
+DUR_MIN     = int(cfg(CFG, ('logic', 'duration_min_ms'), 0))                    #Duration_minimum_seconds(0:OFF)
+DUR_MAX     = int(cfg(CFG, ('logic', 'duration_max_ms'), 0))                    #Duration_maximum_seconds(0:OFF)
 WRITE_GUARD = bool(cfg(CFG, ('logic', 'write_guard_enable'), False))
